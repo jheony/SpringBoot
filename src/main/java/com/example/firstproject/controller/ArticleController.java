b00975506af46bc8a8e76846cd39a53a83a50618
JiHyeon Yun <heon__y@naver.com>
2024-03-13 19:03:48 +0900

    delete

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,7 +43,7 @@ public class ArticleController {
     }
 
     @GetMapping("/articles/{id}")  //데이터 조회 요청 접수
-    public String show(@PathVariable Long id, Model model){
+    public String show(@PathVariable Long id, Model model) {
         log.info("id = " + id);
         //1. id를 조회해 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
@@ -55,7 +56,7 @@ public class ArticleController {
     }
 
     @GetMapping("/articles")
-    public String index(Model model){
+    public String index(Model model) {
         //1. 모든 데이터 가져오기
         ArrayList<Article> articleEntityList = articleRepository.findAll();
 
@@ -67,7 +68,7 @@ public class ArticleController {
     }
 
     @GetMapping("/articles/{id}/edit")
-    public String edit(@PathVariable Long id, Model model){
+    public String edit(@PathVariable Long id, Model model) {
         //수정할 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
         //모델에 데이터 등록하기
@@ -75,8 +76,9 @@ public class ArticleController {
         //뷰페이지 설정하기
         return "articles/edit";
     }
+
     @PostMapping("/articles/update")
-    public String update(ArticleForm form){
+    public String update(ArticleForm form) {
         log.info(form.toString());
         //1. DTO를 엔티티로 변환
         Article articleEntity = form.toEntity();
@@ -87,10 +89,25 @@ public class ArticleController {
         Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
 
         //2-2. 기존 데이터 값 갱신
-        if (target != null){
+        if (target != null) {
             articleRepository.save(articleEntity);
         }
         //3.수정 결과 페이지로 리다이렉트
-        return "redirect:/articles/"+articleEntity.getId();
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다!!");
+//        1. 삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+//        2. 대상 엔티티 삭제하기
+        if (target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제됐습니다!");
+        }
+//        3. 결과 페이지로 리다이렉트하기
+        return "redirect:/articles";
     }
 }
