5d9690fe591944a3a69e049714faabbaaef2da44
JiHyeon Yun <heon__y@naver.com>
2024-03-13 16:29:50 +0900

    memberEdit

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -33,21 +33,51 @@ public class MemberController {
 
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
-        return "redirect:members/"+saved.getId();
+        return "redirect:members/" + saved.getId();
     }
 
     @GetMapping("/members/{id}")
-    public String show(@PathVariable Long id, Model model){
+    public String show(@PathVariable Long id, Model model) {
         Member memberEntity = memberRepository.findById(id).orElse(null);
         model.addAttribute("member", memberEntity);
         return "members/show";
     }
 
     @GetMapping("/members")
-    public String index(Model model){
+    public String index(Model model) {
         ArrayList<Member> memberArrayList = memberRepository.findAll();
-        model.addAttribute("memberList",memberArrayList);
+        model.addAttribute("memberList", memberArrayList);
         return "members/index";
+    }
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        //수정할 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        //모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        //뷰페이지 설정하기
+        return "/members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String updaate(MemberForm form) {
+        log.info(form.toString());
+
+        //1. DTO를 엔티티로 변환
+        Member memberEntity = form.toEntity();
+        log.info(memberEntity.toString());
+
+        //2. 엔티티를 DB에 저장
+        //2-1. DB에서 기존 데이터 가져오기
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+
+        //2-2. 기존 데이터 값 갱신
+        if (target != null) {
+            memberRepository.save(memberEntity);
+        }
+        //3.수정 결과 페이지로 리다이렉트
+        return "redirect:/members/" + memberEntity.getId();
     }
 
 }
--- src/main/java/com/example/firstproject/dto/MemberForm.java
+++ src/main/java/com/example/firstproject/dto/MemberForm.java
@@ -9,8 +9,9 @@ import lombok.ToString;
 public class MemberForm {
     private String email;
     private String password;
+    private Long id;
 
     public Member toEntity(){
-        return new Member(null, email, password);
+        return new Member(id, email, password);
     }
 }
