4f091cf704629c949c7aba40f32aec78c2736eb5
JiHyeon Yun <heon__y@naver.com>
2024-03-13 19:40:07 +0900

    memberDelete

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO article(id, title, content) VALUES(1,'가','1111');
 INSERT INTO article(id, title, content) VALUES(2,'나','2222');
 INSERT INTO article(id, title, content) VALUES(3,'다','3333');
+
+INSERT INTO member(id, email, password) VALUES(1,'a@naver.com','aaa');
+INSERT INTO member(id, email, password) VALUES(2,'b@naver.com','bbb');
+INSERT INTO member(id, email, password) VALUES(3,'c@naver.com','ccc');
