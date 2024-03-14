9dc52d93563b02638cf4bd378b9cc69edac6b2f7
JiHyeon Yun <heon__y@naver.com>
2024-03-14 20:29:58 +0900

    api

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
-INSERT INTO article(id, title, content) VALUES(1,'가','1111');
-INSERT INTO article(id, title, content) VALUES(2,'나','2222');
-INSERT INTO article(id, title, content) VALUES(3,'다','3333');
+INSERT INTO article( title, content) VALUES('가','1111');
+INSERT INTO article( title, content) VALUES('나','2222');
+INSERT INTO article( title, content) VALUES('다','3333');
 
 INSERT INTO member(id, email, password) VALUES(1,'a@naver.com','aaa');
 INSERT INTO member(id, email, password) VALUES(2,'b@naver.com','bbb');
 INSERT INTO member(id, email, password) VALUES(3,'c@naver.com','ccc');
+
