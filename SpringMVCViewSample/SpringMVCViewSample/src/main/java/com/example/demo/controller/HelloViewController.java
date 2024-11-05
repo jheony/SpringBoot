1304472ddb800ccc677f69c449ba2d7670f89b8b
윤지현 <139779783+jheony@users.noreply.github.com>
2024-11-05 14:56:08 +0900

    MVC

---
+++ SpringMVCViewSample/SpringMVCViewSample/src/main/java/com/example/demo/controller/HelloViewController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("hello")
+public class HelloViewController {
+    @GetMapping("view")
+    public String helloView(){
+        // 뷰의 이름 반환
+        return "hello";
+    }
+}
+
